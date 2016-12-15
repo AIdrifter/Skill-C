1959fa1dd039e79af40deefe2b816d47f1a6da2f
AIdrifter <aidrifter@gmail.com>
2016-12-15 11:59:20 +0800

    [Add] check partion free size
    check partion free size

--- file/read_write.c
+++ file/read_write.c
@@ -8,14 +8,23 @@ int main()
 {
     int ret=0;
     FILE * pFile;
-    pFile = fopen ("myfile.txt","wb+");
+    char buffer[100];
+    pFile = fopen ("myfile.txt","rwb+");
     ret = fwrite ("fopen example",sizeof(char),15,pFile);
     fprintf(stderr,"%d %s  ret %d\n",errno,strerror(errno),ret);
+
+    ret = fread (buffer,1,5,pFile);
+    printf(".......... %s \n",buffer);
 
     ret = unlink("myfile.txt");
     fprintf(stderr,"%d %s  ret %d\n",errno,strerror(errno),ret);
 
     ret = fwrite ("fopen example",sizeof(char),15,pFile);
+    fprintf(stderr,"%d %s  ret %d\n",errno,strerror(errno),ret);
+
+    ret = fread (buffer,1,20,pFile);
+    printf(".......... %s \n",buffer);
+
     fprintf(stderr,"%d %s  ret %d\n",errno,strerror(errno),ret);
 
     fclose(pFile);
